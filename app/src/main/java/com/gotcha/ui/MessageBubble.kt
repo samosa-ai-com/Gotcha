fa99c7185cce469f6e8c2768a654a92f9177433d
Rishabh Bajpai <rishabhbajpai24@gmail.com>
2026-07-11 02:28:31 -0500

    refactor: remove animateContentSize modifier from MessageBubble layout

--- app/src/main/java/com/gotcha/ui/MessageBubble.kt
+++ app/src/main/java/com/gotcha/ui/MessageBubble.kt
@@ -1,6 +1,5 @@
 package com.gotcha.ui
 
-import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -74,7 +73,6 @@ fun MessageBubble(
             Column(
                 modifier = Modifier
                     .widthIn(max = 320.dp)
-                    .animateContentSize()
                     .then(if (isTool) Modifier.clickable { expanded.value = !expanded.value } else Modifier)
                     .padding(16.dp)
             ) {
