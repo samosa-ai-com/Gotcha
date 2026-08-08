44ac505f8c007376c78300a9e89fe003b840c8a0
Rishabh Bajpai <rishabhbajpai24@gmail.com>
2026-08-08 14:02:16 -0500

    chore: bump project version from 0.1.0 to 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
         minSdk = 30
         targetSdk = 34
         versionCode = 1
-        versionName = "0.1.0"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
